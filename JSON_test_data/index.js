5089eeb266ca9bc2f5f71f8cfe93d1c76dcffe6f
Michael Lee <michael@Michaels-MacBook-Pro-2.local>
2018-10-01 15:45:41 -0700

    Updates

--- JSON_test_data/index.js
+++
@@ -1,4 +0,0 @@
-import mockRequest from './mock_query_request';
-import mockData from './mock_query_data';
-
-export { mockData, mockRequest };
--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'demo/containers/App';
-
-import "./lib/style/Portal.css";
-
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
